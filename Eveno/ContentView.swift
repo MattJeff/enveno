a2cab251bda0637de2edc341f8547efb358ff30a
MattJeff <76613302+MattJeff@users.noreply.github.com>
2022-10-04 19:32:42 +0200

    Add files via upload

---
+++ Eveno/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Eveno
+//
+//  Created by Mathis Higuinen on 04/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eveno/EvenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvenoApp.swift
+//  Eveno
+//
+//  Created by Mathis Higuinen on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct EvenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
