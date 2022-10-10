9c285e43d85d04e99e3c46db396c49a74ed2cc67
AlexRepaire <repaire.alexandre@gmail.com>
2022-10-10 17:36:43 +0200

    [feat] creation vue singin et signup

--- Eveno/Eveno/View/begin.swift
+++ Eveno/Eveno/View/begin.swift
@@ -16,7 +16,7 @@ struct begin: View {
                 Image(systemName: "arrow.left")
                 .frame(maxWidth:.infinity,alignment: .leading)
             
-            Image("image")
+            Image("illustration-3")
                 .resizable()
                 .aspectRatio( contentMode: .fill)
                 .frame(width:size.width/2,height:size.width/2)
