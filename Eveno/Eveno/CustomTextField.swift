41961b710f3f57cf11ba4a55b3e193c6ef9d77cc
MattJeff <mhiguinen235@gmail.com>
2022-10-10 19:13:24 +0200

    SignUp View Modification

--- Eveno/Eveno/CustomTextField.swift
+++ Eveno/Eveno/CustomTextField.swift
@@ -2,23 +2,45 @@
 //  CustomTextField.swift
 //  Eveno
 //
+//  Created by Mathis Higuinen on 10/10/2022.
+//
+
+
+//
+//  CustomTextField.swift
+//  Eveno
+//
 //  Created by Alexandre Repaire-Carlier on 07/10/2022.
 //
+
+
+
+enum TextFieldType{
+    case secure
+    case standar
+}
 
 import SwiftUI
 
 struct CustomTextField:View{
     @Binding var text: String
     var placeholder:String = "Email"
-    var image:String = ""
+    var image:String = "Message"
     var secure:Bool = false
-
+   
     var body:some View{
 
         HStack{
-            Image(systemName: image)
-                .foregroundColor(.gray)
-            if(secure == true){
+            
+            
+            Image("\( image)")
+                .renderingMode(.template)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:20,height: 20)
+                .foregroundColor(Color("500"))
+            
+            if secure{
                 SecureField("\(placeholder)", text: $text)
                     .foregroundColor(.black)
                 Image(systemName: "eye.slash.fill")
@@ -29,15 +51,25 @@ struct CustomTextField:View{
             }
 
                 
-        }
-        .padding()
+        }.padding()
+        .background(Color("50"))
+        .cornerRadius(16)
+    
           }
 }
+
+
+
+
+
+
+
 
 struct CustomTextField_Previews: PreviewProvider {
 
     static var previews: some View {
         CustomTextField(text: .constant("test"))
+            .previewLayout(.sizeThatFits)
     }
 }
 
---
+++ Eveno/Eveno/View/Autentification/Signup.swift
@@ -0,0 +1,137 @@
+//
+//  Signup.swift
+//  Eveno
+//
+//  Created by Alexandre Repaire-Carlier on 05/10/2022.
+//
+
+import SwiftUI
+
+struct Signup: View {
+    @State private var email:String = ""
+    @State private var password:String = ""
+    @State private var toggleRemember = false
+    @State var selected = "facebook"
+    var logo = ["facebook","apple","google"]
+    var size = UIScreen.main.bounds
+
+    var body: some View {
+        VStack{
+            Image(systemName: "arrow.left")
+            .frame(maxWidth:.infinity,alignment: .leading)
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.width/2, height: size.width/2)
+            Text("Create New Account")
+                .bold()
+                .font(.title)
+            CustomTextField(text: $email, placeholder: "Email", image: "Message")
+                .padding(.top)
+            
+            CustomTextField(text: $password, placeholder: "Password", image: "Lock", secure: true)
+            
+            HStack(spacing:16){
+                Button {
+                    toggleRemember.toggle()
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(lineWidth:5)
+                            .frame(width:24,height: 24)
+                            .foregroundColor(Color("primary"))
+                        if toggleRemember{
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(width:24,height: 24)
+                                .foregroundColor(Color("primary"))
+                            Image(systemName: "checkmark")
+                                .bold()
+                                .foregroundColor(.white)
+                               
+                        }
+                    }
+                        
+                }
+                
+                Text("Rememeber me")
+                    .bold()
+
+                
+            }.padding(.vertical)
+
+            CustomButton(text: "Sign up").padding(.vertical)
+            
+            HStack(spacing:16){
+                Rectangle()
+                    .frame(width: 96,height: 1)
+                    .foregroundColor(Color(.systemGray3))
+                
+                Text("or continue with")
+                    .foregroundColor(Color("700"))
+                    .bold()
+                    .font(.system(size: 18))
+                    
+                Rectangle()
+                    .frame(width: 96,height: 1)
+                    .foregroundColor(Color(.systemGray3))
+            }.padding(.vertical)
+            
+            HStack{
+                Spacer()
+                
+                
+                ForEach(logo,id:\.self) { valeur in
+                    Spacer()
+                 
+                        Image("\(valeur)")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 20, height: 30)
+                            .padding()
+                            .padding(.horizontal)
+                            .background(
+                             RoundedRectangle(cornerRadius: 16)
+                                .stroke(Color("200"),lineWidth: 2)
+                            )
+                           
+                
+                    Spacer()
+                }
+          
+                
+             
+            }.padding()
+            HStack{
+                Text("Already have an account? ")
+                Text("Sign in")
+                    .foregroundColor(Color("bleu"))
+                    .bold()
+                
+            }.padding()
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+        }
+        .padding()
+        
+
+    }
+}
+
+struct buttonClicker: ViewModifier {
+    var clicker:Bool
+    func body(content: Content) -> some View {
+     
+            content
+            .padding()
+        
+       
+    }
+}
+
+
+struct Signup_Previews: PreviewProvider {
+    static var previews: some View {
+        Signup()
+    }
+}
+
