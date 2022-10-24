440d55f84f32c6f9502a96ac605c6bf242f14305
MattJeff <mhiguinen235@gmail.com>
2022-10-24 20:13:56 +0200

    feat:[78_Light_profil] vue profile disign terminé

---
+++ Eveno/Eveno/View/MenuView.swift
@@ -0,0 +1,48 @@
+//
+//  MenuView.swift
+//  Eveno
+//
+//  Created by Mathis Higuinen on 24/10/2022.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+     
+            VStack(spacing:24){
+                ForEach(Menue.allCases) { element in
+                    
+                    if element.rawValue == "Language"{
+                        NavigationLink {
+                            Text("Langue")
+                        } label: {
+                            MenuCellLanguage(image: element.rawValue, title: element.rawValue, langue: "English (US)")
+                        }
+
+                       
+                    }else if element.rawValue == "Dark Mode"{
+                        
+         
+                        MenuCellDarkMode(image:element.rawValue,title: element.rawValue)
+                        
+                    }else{
+                        NavigationLink {
+                            Text(element.rawValue)
+                        } label: {
+                            MenuCell(image:element.rawValue,title:element.rawValue)
+                        }
+                    }
+                }
+            }
+        
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
+
+
--- Eveno/Eveno/View/ProfileView.swift
+++ Eveno/Eveno/View/ProfileView.swift
@@ -9,18 +9,66 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack{
-            HStack{
-                Image("logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width:28,height: 28)
-                Text("Profile")
+        NavigationView{
+            ScrollView(showsIndicators:false){
+                VStack{
+                    HStack{
+                        Image("logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width:50,height:50)
+                        
+                        Text("Profile")
+                            .modifier(TextSizeModifier(size: .h4, Ufont: .Bold))
+                            .foregroundColor(Color("900"))
+                        Spacer()
+                        Image("Auto Layout Horizontal")
+                            .renderingMode(.template)
+                        
+                        
+                    }
+                    Image("Image-8")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width:140,height:140)
+                        .clipShape(Circle())
+                    Text("Andrew Ainsley")
+                        .modifier(TextSizeModifier(size: .h3, Ufont: .Bold))
+                    Divider()
+                    HStack{
+                        VStack(spacing:12){
+                            Text("12")
+                                .modifier(TextSizeModifier(size: .h3, Ufont: .Bold))
+                            Text("Event")
+                                .modifier(TextSizeModifier(size: .bodyLarge, Ufont: .Medium))
+                                .foregroundColor(Color("700"))
+                            
+                        }.padding()
+                        Divider().frame(height:72)
+                        VStack(spacing:12){
+                            Text("7,389")
+                                .modifier(TextSizeModifier(size: .h3, Ufont: .Bold))
+                            Text("Followers")
+                                .foregroundColor(Color("700"))
+                            
+                        }.padding()
+                        Divider().frame(height:72)
+                        VStack(spacing:12){
+                            Text("125")
+                                .modifier(TextSizeModifier(size: .h3, Ufont: .Bold))
+                            Text("Following")
+                                .foregroundColor(Color("700"))
+                            
+                        }.padding()
+                    }
+                    Divider()
+                    MenuView()
+    
+                    Spacer()
                     
-                    
+                }.padding()
             }
-            
-        }.padding()
+        }
     }
 }
 
